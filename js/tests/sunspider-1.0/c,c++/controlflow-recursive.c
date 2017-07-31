4522bbf45772b5d141cf88e11d6439faff3acd63
Apurva Raman <t-aprama@microsoft.com>
2017-07-31 13:42:31 -0700

    Adding c benchmarks.

---
+++ js/tests/sunspider-1.0/c,c++/controlflow-recursive.c
@@ -0,0 +1,27 @@
+int ack(int m, int n){
+   if (m==0) { return n+1; }
+   if (n==0) { return ack(m-1,1); }
+   return (ack(m-1, ack(m,n-1)));
+}
+
+float fib(float n) {
+    if (n < 2){ return 1; }
+    return (fib(n-2) + fib(n-1));
+}
+
+int tak(int x, int y, int z) {
+    if (y >= x) return z;
+    return (tak(tak(x-1,y,z), tak(y-1,z,x), tak(z-1,x,y)));
+}
+
+int main(){
+    int i;
+    // setting return values of functions and summing them added so that binaryen does not optimize away function calls
+    float a, b, c; 
+    for (i = 3; i <=5; i++){
+        a += ack(3, i);
+        b += fib(17.0+i);
+        c += tak(3*i+3, 2*i+2, i+1);
+    }
+    return a + b + c;
+}
---
+++ js/tests/sunspider-1.0/c,c++/math-cordic.c
@@ -0,0 +1,92 @@
+/*
+ * Copyright (C) Rich Moore.  All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY CONTRIBUTORS ``AS IS'' AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL APPLE COMPUTER, INC. OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
+ * OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. 
+ */
+
+/////. Start CORDIC
+
+float AG_CONST = 0.6072529350;
+
+float FIXED(float X)
+{
+  return (X * 65536.0);
+}
+
+float FLOAT(int X)
+{
+  return (X / 65536.0);
+}
+
+float DEG2RAD(float X)
+{
+  return (0.017453 * (X));
+}
+
+const float Angles[12] = {(45.0* 65536.0),(26.565* 65536.0),(14.0362* 65536.0), (7.12502* 65536.0),
+  (3.57633* 65536.0), (1.78991* 65536.0), (0.895174* 65536.0), (0.447614* 65536.0), (0.223811* 65536.0), (0.111906* 65536.0), (0.055953* 65536.0),(0.027977* 65536.0) };
+
+float Target = 28.027;
+
+float cordicsincos(float Target) {
+    int X;
+    int Y;
+    float TargetAngle;
+    float CurrAngle;
+    int Step;
+ 
+    X = FIXED(AG_CONST);         /* AG_CONST * cos(0) */
+    Y = 0;                       /* AG_CONST * sin(0) */
+
+    TargetAngle = FIXED(Target);
+    CurrAngle = 0;
+    for (Step = 0; Step < 12; Step++) {
+        float NewX;
+        if (TargetAngle > CurrAngle) {
+            NewX = X - (Y >> Step);
+            Y = (X >> Step) + Y;
+            X = NewX;
+            CurrAngle += Angles[Step];
+        } else {
+            NewX = X + (Y >> Step);
+            Y = -(X >> Step) + Y;
+            X = NewX;
+            CurrAngle -= Angles[Step];
+        }
+    }
+
+    return FLOAT(X) * FLOAT(Y);
+}
+
+///// End CORDIC
+
+float total = 0;
+
+int main(){
+    int i;
+    int runs = 25000;
+    for (i = 0; i <runs; i++){
+        total += cordicsincos(Target);
+    } 
+    return total;
+    // total not returned in original, time elapsed returned instead.
+}
