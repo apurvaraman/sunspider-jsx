4598bb4422be9a7f8c1d733b28bc8aa325cfa456
Apurva Raman <t-aprama@microsoft.com>
2017-07-31 14:35:39 -0700

    Adding nsieve c benchmark.

---
+++ js/tests/sunspider-1.0/c,c++/access-nsieve.c
@@ -0,0 +1,27 @@
+//removed pad
+
+int nsieve(int m, int*isPrime){
+   int i, k, count;
+
+   for (i=2; i<=m; i++) { isPrime[i] = 1; }
+   count = 0;
+
+   for (i=2; i<=m; i++){
+      if (isPrime[i]) {
+         for (k=i+i; k<=m; k+=i) {isPrime[k] = 0};
+         count++;
+      }
+   }
+   return count;
+}
+
+int main() {
+    int i;
+    int retVal;
+    for (i = 1; i <= 3; i++ ) {
+        int m = (1<<i)*10000;
+        int flags [m+1];
+        retVal += nsieve(m, flags); // changed return value of main        
+    }
+    return retVal;
+}
